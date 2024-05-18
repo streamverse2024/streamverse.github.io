1888ba99c94ab15eb6b79113cbded1a53117f9b6
Diego silva <diego.h.n.silva@gmail.com>
2024-05-18 18:36:17 -0300

    Atualizacao no footer

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 
 Somos uma plataforma de streaming que oferece uma ampla variedade de conteúdo de entretenimento, incluindo filmes, séries de TV, documentários, programas originais e eventos ao vivo. Nosso objetivo é proporcionar uma experiência de entretenimento sob demanda, acessível e personalizada para os nossos usuários.
 
+
+## Demonstração
+
+
+- [Home (https://streamverse2024.github.io/)]
+
+
+## Autores
+
+- [@eduardo](https://www.github.com/)
+- [@marivaldo](https://www.github.com/)
+- [@pedro](https://www.github.com/)
+- [@diego](https://www.github.com/)
+
--- scripts/script_menu.js
+++ scripts/script_menu.js
@@ -7,7 +7,6 @@ document.addEventListener('scroll', function() {
     }
 });
 
-
 handleSetLayoutDefaultMenu  = () => {
     let containerMenu       = document.querySelector('#div-container-menu');
     let logoMenu            = document.querySelector('#img-logo-menu');
@@ -36,7 +35,6 @@ handleSetLayoutOutMenu  = () => {
 
 let itemsMenu       = document.querySelectorAll('.div-item-menu');
 let containerMenu   = document.querySelector('#div-container-menu');
-
 
 itemsMenu.forEach((item) => {
     item.addEventListener('click', function() {
@@ -80,7 +78,6 @@ btnVerPlanos.addEventListener('click', function() {
     });
 });
 
-
 let btnSaibaMais = document.querySelector('#div-btn-saiba-mais');
 btnSaibaMais.addEventListener('click', function() {
     let idSection       = 'section-planos';
@@ -91,5 +88,53 @@ btnSaibaMais.addEventListener('click', function() {
     });
 });
 
+let logoFooter      = document.querySelector('#div-logo-footer');
+logoFooter.addEventListener('click', function() {
+    window.location.reload();
+});
 
+let itemsMenuFooter = document.querySelectorAll('.span-iten-link-footer');
+
+itemsMenuFooter.forEach((item) => {
+    item.addEventListener('click', function() {
+        let idSection       = this.dataset.sectionValue;
+        var elementSection  = document.getElementById(idSection); 
+        elementSection.scrollIntoView({
+            behavior    : "smooth", 
+            block       : "start"
+        });
+    });
+});
+
+
+let socialMediaMenu = document.querySelectorAll('.div-container-social-media');
+
+socialMediaMenu.forEach((item) => {
+    item.addEventListener('click', function() {
+        let targetValue = this.dataset.navigate;
+        if(targetValue === 'facebook'){
+            window.open('https://www.facebook.com/', '_blank');
+        }
+
+        if(targetValue === 'instagram'){
+            window.open('https://www.instagram.com/', '_blank');
+        }
+
+        if(targetValue === 'twitter'){
+            window.open('https://x.com/', '_blank');
+        }
+
+        if(targetValue === 'telegram'){
+            window.open('https://telegram.org/', '_blank');
+        }
+
+        if(targetValue === 'whatsapp'){
+            window.open('https://www.whatsapp.com/', '_blank');
+        }
+
+        if(targetValue === 'youtube'){
+            window.open('https://www.youtube.com/', '_blank');
+        }
+    });
+});
 
