0f9f82cbed64bf3ee60e11bf2333dbc752464498
Diego silva <diego.h.n.silva@gmail.com>
2024-05-20 18:25:00 -0300

    Adicao da section noticias

--- scripts/script_sections.js
+++ scripts/script_sections.js
@@ -1,10 +1,15 @@
+$(window).on('beforeunload', function () {
+    $(window).scrollTop(0);
+});
+
 let btnVerMais = document.querySelector('#div-btn-ver-mais');
 
-btnVerMais.addEventListener('click', function() {
-    let idSection       = 'section-sobre-catalogo';
-    var elementSection  = document.getElementById(idSection); 
+btnVerMais.addEventListener('click', function () {
+    let idSection = 'section-sobre-detalhes';
+    var elementSection = document.getElementById(idSection);
     elementSection.scrollIntoView({
-        behavior    : "smooth", 
-        block       : "start"
+        behavior: "smooth",
+        block: "start"
     });
 });
+
